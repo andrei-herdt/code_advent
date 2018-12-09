b10d4633693b3c41d5197e4c6c3dcf0d62783cfe
andrei <andrei.herdt@gmail.com>
2018-12-09 20:43:15 +0100

    Optimize find_duplicate

--- src/find_duplicate.cpp
+++ src/find_duplicate.cpp
@@ -3,16 +3,12 @@
 using namespace std;
 
 string FindDuplicate(const vector<string>& words) {
-    for (size_t i = 0; i < words[0].size(); ++i) {
-        vector<string> shortened_words(words);
-        for (auto& word : shortened_words) {
-            word[i] = '*';
-        }
+    for (size_t col{0}; col < words.front().size(); ++col) {
         map<string, size_t> word_map;
-        string duplicate_word;
-        for (auto& word : shortened_words) {
+        for (auto word : words) {
+            word.erase(col, 1);
             if (++word_map[word] == 2) {
-                return word.erase(i, 1);
+                return word;
             }
         }
     }
