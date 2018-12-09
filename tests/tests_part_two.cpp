ab8d5a2e3f89fa5bbb075d3e208d12848278aedf
andrei <andrei.herdt@gmail.com>
2018-12-09 17:30:07 +0100

    Day two part two

--- tests/tests_part_two.cpp
+++ tests/tests_part_two.cpp
@@ -4,16 +4,24 @@
 
 #include "pairs_and_triples.h"
 
-TEST(part_two, same_letter) {
-    std::string word{"abcdef"};
-    std::sort(std::begin(word), std::end(word));
+using namespace std;
+
+TEST(day_two, part_two) {
+    const vector<string> words{"abcde", "fghij", "klmno", "pqrst",
+                               "fguij", "axcye", "wvxyz"};
+    vector<string> shortened_words(words);
+}
+
+TEST(day_two, part_one) {
+    string word{"abcdef"};
+    sort(begin(word), end(word));
 
     int two_letters{0};
     int three_letters{0};
     ASSERT_FALSE(two_letters == 1);
     ASSERT_FALSE(three_letters == 1);
 
-    std::vector<std::string> words;
+    vector<string> words;
     word = "bababc";
     words.push_back(word);
     FindPairsAndTriples(word, two_letters, three_letters);
@@ -52,7 +60,7 @@ TEST(part_two, same_letter) {
 
     int number_two_letter_words = 0;
     int number_three_letter_words = 0;
-    for (std::string word : words) {
+    for (string word : words) {
         FindPairsAndTriples(word, two_letters, three_letters);
         number_two_letter_words += two_letters;
         number_three_letter_words += three_letters;
