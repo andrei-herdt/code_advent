912cf4b0c54600ab4dff7238ca826fee99103731
andrei <andrei.herdt@gmail.com>
2018-12-13 20:12:07 +0100

    Fix regex

--- src/day_three_utils.cpp
+++ src/day_three_utils.cpp
@@ -1,10 +1,11 @@
 #include "day_three_utils.h"
 
+#include <iostream>
 #include <regex>
 
 std::vector<size_t> ExtractClaim(const std::string& claim) {
     std::vector<size_t> claim_vec;
-    std::regex id_regex("([0-9])");
+    std::regex id_regex("([0-9]+)");
     auto words_begin =
         std::sregex_iterator(claim.begin(), claim.end(), id_regex);
     auto words_end = std::sregex_iterator();
@@ -18,9 +19,13 @@ std::vector<size_t> ExtractClaim(const std::string& claim) {
 
 void MakeClaim(const std::vector<size_t>& claim,
                std::map<std::pair<int, int>, size_t>& fabric) {
-    for (int i = 0; i < claim[3]; ++i) {
-        for (int j = 0; j < claim[4]; ++j) {
-            ++fabric[std::make_pair(claim[1] + i, claim[2] + j)];
+    size_t x{claim[1]};
+    size_t y{claim[2]};
+    size_t width{claim[3]};
+    size_t height{claim[4]};
+    for (size_t w = 0; w < width; ++w) {
+        for (size_t h = 0; h < height; ++h) {
+            ++fabric[std::make_pair(x + w, y + h)];
         }
     }
 }
