0f5cdc0f0d2b01b06315d9abbe8ddafc8d99738a
andrei <andrei.herdt@gmail.com>
2018-12-22 15:15:46 +0100

    Refactor

--- tests/tests_day_four.cpp
+++ tests/tests_day_four.cpp
@@ -1,19 +1,33 @@
 #include <gmock/gmock.h>
 #include <regex>
 
+std::string ExtractFirstOccurence(const std::string& word,
+                                  const std::string& pattern) {
+    std::regex id_regex(pattern);
+    auto words_begin = std::sregex_iterator(word.begin(), word.end(), id_regex);
+    auto words_end = std::sregex_iterator();
+    return words_begin->str();
+}
+
 TEST(day_four, part_one) {
     const std::vector<std::string> foo{
         "[1518 - 11 - 01 00:00] Guard #10 begins shift",
-        "[1518 - 11 - 01 00:05] falls asleep [1518 - 11 - 01 00:25] wakes up",
-        "[1518 - 11 - 01 00:30] falls asleep [1518 - 11 - 01 00:55] wakes up",
+        "[1518 - 11 - 01 00:05] falls asleep [1518 - 11 - 01 00:25] wakes "
+        "up",
+        "[1518 - 11 - 01 00:30] falls asleep [1518 - 11 - 01 00:55] wakes "
+        "up",
         "[1518 - 11 - 01 23:58] Guard #99 begins shift",
-        "[1518 - 11 - 02 00:40] falls asleep [1518 - 11 - 02 00:50] wakes up",
+        "[1518 - 11 - 02 00:40] falls asleep [1518 - 11 - 02 00:50] wakes "
+        "up",
         "[1518 - 11 - 03 00:05] Guard #10 begins shift",
-        "[1518 - 11 - 03 00:24] falls asleep [1518 - 11 - 03 00:29] wakes up",
+        "[1518 - 11 - 03 00:24] falls asleep [1518 - 11 - 03 00:29] wakes "
+        "up",
         "[1518 - 11 - 04 00:02] Guard #99 begins shift",
-        "[1518 - 11 - 04 00:36] falls asleep [1518 - 11 - 04 00:46] wakes up",
+        "[1518 - 11 - 04 00:36] falls asleep [1518 - 11 - 04 00:46] wakes "
+        "up",
         "[1518 - 11 - 05 00:03] Guard #99 begins shift",
-        "[1518 - 11 - 05 00:45] falls asleep [1518 - 11 - 05 00:55] wakes up"};
+        "[1518 - 11 - 05 00:45] falls asleep [1518 - 11 - 05 00:55] wakes "
+        "up"};
 
     /*
     Find guard with most sleeping time
@@ -25,17 +39,9 @@ TEST(day_four, part_one) {
      Sum of sleeping minutes for each minute over all instances of specific
      id Take max -> minute
     */
-    std::multimap < std::pair for (auto word : foo) {
-        std::regex id_regex("#([0-9]+)");
-        auto words_begin =
-            std::sregex_iterator(word.begin(), word.end(), id_regex);
-        auto words_end = std::sregex_iterator();
-
-        if (std::distance(words_begin, words_end)) {
-            for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-                std::smatch match = *i;
-            }
-        };
+    std::multimap<std::pair<size_t, size_t>, size_t> sleep_minutes;
+    for (auto word : foo) {
+        ExtractFirstOccurence(word, "#([0-9]+)");
     }
 
     int id = 10;
