4649ca7ec6c4a52b199964149f916ef9a75dc6b0
andrei <andrei.herdt@gmail.com>
2018-12-22 11:46:18 +0100

    Day four

--- tests/tests_day_four.cpp
+++ tests/tests_day_four.cpp
@@ -1,17 +1,18 @@
 #include <gmock/gmock.h>
+#include <regex>
 
 TEST(day_four, part_one) {
-    const std::vector<std::string> claims{
-        "[1518 - 11 - 01 00:00] Guard #10 begins shift"
-        "[1518 - 11 - 01 00:05] falls asleep [1518 - 11 - 01 00:25] wakes up"
-        "[1518 - 11 - 01 00:30] falls asleep [1518 - 11 - 01 00:55] wakes up"
-        "[1518 - 11 - 01 23:58] Guard #99 begins shift"
-        "[1518 - 11 - 02 00:40] falls asleep [1518 - 11 - 02 00:50] wakes up"
-        "[1518 - 11 - 03 00:05] Guard #10 begins shift"
-        "[1518 - 11 - 03 00:24] falls asleep [1518 - 11 - 03 00:29] wakes up"
-        "[1518 - 11 - 04 00:02] Guard #99 begins shift"
-        "[1518 - 11 - 04 00:36] falls asleep [1518 - 11 - 04 00:46] wakes up"
-        "[1518 - 11 - 05 00:03] Guard #99 begins shift"
+    const std::vector<std::string> foo{
+        "[1518 - 11 - 01 00:00] Guard #10 begins shift",
+        "[1518 - 11 - 01 00:05] falls asleep [1518 - 11 - 01 00:25] wakes up",
+        "[1518 - 11 - 01 00:30] falls asleep [1518 - 11 - 01 00:55] wakes up",
+        "[1518 - 11 - 01 23:58] Guard #99 begins shift",
+        "[1518 - 11 - 02 00:40] falls asleep [1518 - 11 - 02 00:50] wakes up",
+        "[1518 - 11 - 03 00:05] Guard #10 begins shift",
+        "[1518 - 11 - 03 00:24] falls asleep [1518 - 11 - 03 00:29] wakes up",
+        "[1518 - 11 - 04 00:02] Guard #99 begins shift",
+        "[1518 - 11 - 04 00:36] falls asleep [1518 - 11 - 04 00:46] wakes up",
+        "[1518 - 11 - 05 00:03] Guard #99 begins shift",
         "[1518 - 11 - 05 00:45] falls asleep [1518 - 11 - 05 00:55] wakes up"};
 
     /*
@@ -24,6 +25,18 @@ TEST(day_four, part_one) {
      Sum of sleeping minutes for each minute over all instances of specific
      id Take max -> minute
     */
+    std::multimap < std::pair for (auto word : foo) {
+        std::regex id_regex("#([0-9]+)");
+        auto words_begin =
+            std::sregex_iterator(word.begin(), word.end(), id_regex);
+        auto words_end = std::sregex_iterator();
+
+        if (std::distance(words_begin, words_end)) {
+            for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+                std::smatch match = *i;
+            }
+        };
+    }
 
     int id = 10;
     int minute = 24;
