37e9f55db817ceb712f66d0e701564b2d37e6fa3
andrei <andrei.herdt@gmail.com>
2019-01-02 10:15:42 +0100

    Start 12

--- src/11.cpp
+++ src/11.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <fstream>
 #include <iostream>
 #include <regex>
@@ -6,14 +7,28 @@
 using namespace std;
 
 int main() {
+    int serial_number{0};
+    auto power_level = [&serial_number](size_t x, size_t y) {
+        int rack_id = x + 10;
+        int pl = rack_id * y;
+        pl += serial_number;
+        pl *= rack_id;
+        pl = (pl / 100) % 10;
+        pl -= 5;
+        return pl;
+    };
+
+    serial_number = 8;
+    assert(power_level(3, 5) == 4);
+    serial_number = 57;
+    assert(power_level(122, 79) == -5);
+    serial_number = 39;
+    assert(power_level(217, 196) == 0);
+    serial_number = 71;
+    assert(power_level(101, 153) == 4);
+
     /*Generate map
      * Compute are sizes for 3x3 squares
      * Pick the biggest
      */
-
-    // std::cout << "scores: " << std::endl;
-    // for (auto i : scores) {
-    //     std::cout << i << " ";
-    // }
-    // std::cout << std::endl;
 }
---
+++ src/12.cpp
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    /* Get initial state
+     * Update state 20 times
+     *  for all pots look for match using regex
+     * Sum up all plant containing pots
+     */
+
+    // serial_number = 71;
+    // assert(power_level(101, 153) == 4);
+}
