29b933a874cb59354c79f99ce26d50c5d1e6f3ff
andrei <andrei.herdt@gmail.com>
2019-01-02 10:48:30 +0100

    outline day 13

---
+++ src/13.cpp
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    /* Generate map of the road (x,y)->tracktype
+     * Get states vector (x,y,direction) of all cars
+     * sort according x, y
+     * Update position and direction according to order
+     * Check state vector for equal positions
+     */
+
+    // serial_number = 71;
+    // assert(power_level(101, 153) == 4);
+}
