67cad5fe2e9ee345b5bcfc3a02704e02b27b9e9c
andrei <andrei.herdt@gmail.com>
2019-01-04 11:20:02 +0100

    12

--- src/12.cpp
+++ src/12.cpp
@@ -12,7 +12,37 @@ int main() {
      *  for all pots look for match using regex
      * Sum up all plant containing pots
      */
+    vector<string> rulesraw{
+        "...## => #", "..#.. => #", ".#... => #", ".#.#. => #", ".#.## => #",
+        ".##.. => #", ".#### => #", "#.#.# => #", "#.### => #", "##.#. => #",
+        "##.## => #", "###.. => #", "###.# => #", "####. => #"};
+    vector<pair<string, char>> rules;
+    for (auto rule : rulesraw) {
+        rules.push_back(make_pair(rule.substr(0, 5), rule.back()));
+    }
+    string stateraw{"#..#.#..##......###...###"};
+    string state{stateraw};
+    state.insert(begin(state), '.');
+    state.insert(begin(state), '.');
+    state.insert(begin(state), '.');
+    state.insert(end(state), '.');
+    state.insert(end(state), '.');
+    state.insert(end(state), '.');
 
-    // serial_number = 71;
-    // assert(power_level(101, 153) == 4);
+    auto statenew{state};
+    for (int i = 0; i < stateraw.size(); ++i) {
+        auto pots = state.substr(i + 1, 5);
+        for (auto rule : rules) {
+            if (rule.first == pots) {
+                std::cout << "pots" << std::endl;
+                std::cout << pots << std::endl;
+                std::cout << "rule.first" << std::endl;
+                std::cout << rule.first << std::endl;
+                statenew[i + 3] = rule.second;
+            } else {
+                statenew[i + 3] = '.';
+            }
+        }
+    }
+    std::cout << statenew << std::endl;
 }
